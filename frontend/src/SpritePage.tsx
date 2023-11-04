6dc2f17c884a0512aace18b4d1f01df0a19b5cc3
Eric Shields <shieldse01@yahoo.com>
2023-11-04 17:53:01 -0500

    Finished character

--- frontend/src/SpritePage.tsx
+++ frontend/src/SpritePage.tsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import Character from './components/Character'
 
 export default class SpritePage extends Component {
   render() {
     return (
       // make a board that is 16:9 resolution
-      <div className="sprite-page" style={{backgroundColor: 'burlywood', height: '500px', width: '1000px', border: 'solid black'}}>
-        <div style={{backgroundColor: 'black', height: '50px', width: '50px'}}></div>
+      <div className="sprite-page" style={{backgroundColor: 'burlywood', height: '500px', width: '1000px', border: 'solid black', position: 'relative'}}>
+        <Character name="User" sprite={"hi"} startX={0} startY={0}/>
       </div>
     )
   }
