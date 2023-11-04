94703f7cd97b9e7d8bf603c3def78bbaba9fa2a7
vaishunall <vaishunalla@yahoo.com>
2023-11-04 15:32:27 -0500

    Set up Navigation

---
+++ frontend/src/KanbanPage.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class KanbanPage extends Component {
+  render() {
+    return (
+      <div>KanbanPage</div>
+    )
+  }
+}
---
+++ frontend/src/SpritePage.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class SpritePage extends Component {
+  render() {
+    return (
+      <div>SpritePage</div>
+    )
+  }
+}
