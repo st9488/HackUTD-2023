b35020bfc54e16568bd1554851badfc3a2b87e3c
Nicholas Zolton <nicholaszolton+git@gmail.com>
2023-11-04 16:00:59 -0500

    Add board and sprite to SpritePage component

--- frontend/src/SpritePage.tsx
+++ frontend/src/SpritePage.tsx
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 export default class SpritePage extends Component {
   render() {
     return (
-      <div>SpritePage</div>
+      // make a board that is 16:9 resolution
+      <div className="sprite-page" style={{backgroundColor: 'burlywood', height: '500px', width: '1000px', border: 'solid black'}}>
+        <div style={{backgroundColor: 'black', height: '50px', width: '50px'}}></div>
+      </div>
     )
   }
 }
